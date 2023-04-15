0fda023ffd21e845de217d1507cbbaf54214ce03
BillChe <vasos.red.7@gmail.com>
2023-04-15 13:01:29 +0300

    Commit with detect text initial options (java and kotlin) and flashlight option

--- app/src/main/java/com/example/visual_aid_app/WelcomeActivity.java
+++ app/src/main/java/com/example/visual_aid_app/WelcomeActivity.java
@@ -2,8 +2,12 @@ package com.example.visual_aid_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
@@ -13,7 +17,7 @@ import java.util.Locale;
 
 public class WelcomeActivity extends AppCompatActivity {
     TextToSpeech tts;
-    private Button captureBtn;
+    private Button captureBtn,textDetectBtn,textDetectBtnJav,flashtBtn,flashOffBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,66 @@ public class WelcomeActivity extends AppCompatActivity {
                 startActivity(captureIntent);
             }
         });
+        textDetectBtn = findViewById(R.id.textDetectBtn);
+        textDetectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent captureIntent = new Intent(WelcomeActivity.this, CameraView.class);
+                startActivity(captureIntent);
+            }
+        });
+        textDetectBtnJav = findViewById(R.id.textDetectBtnJav);
+        textDetectBtnJav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent captureIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(captureIntent);
+            }
+        });
+        flashtBtn = findViewById(R.id.flashtBtn);
+        flashtBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                    String cameraId = null;
+                    try {
+                        cameraId = camManager.getCameraIdList()[0];
+                        camManager.setTorchMode(cameraId, true);   //Turn ON
+                        flashOffBtn.setVisibility(View.VISIBLE);
+                        flashOffBtn.setEnabled(true);
+                        flashtBtn.setVisibility(View.GONE);
+                        flashtBtn.setEnabled(false);
+
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        flashOffBtn = findViewById(R.id.flashOffBtn);
+        flashOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                    String cameraId = null;
+                    try {
+                        cameraId = camManager.getCameraIdList()[0];
+                        camManager.setTorchMode(cameraId, false);   //Turn OFF
+                        flashOffBtn.setVisibility(View.GONE);
+                        flashOffBtn.setEnabled(false);
+                        flashtBtn.setVisibility(View.VISIBLE);
+                        flashtBtn.setEnabled(true);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+
     }
 
     private void playWelcomeMessage() {
