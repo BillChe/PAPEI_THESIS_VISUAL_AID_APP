489b6fc85723587349f4e9a4d2c6fcec8e2e84c7
BillChe <vasos.red.7@gmail.com>
2023-04-16 21:42:25 +0300

    Commit with better text detection(java), added test buttons for all functionalities, implemented Firebase method for text/document detection for future use, added empty ActivityZoom

--- app/src/main/java/com/example/visual_aid_app/MainActivity.java
+++ app/src/main/java/com/example/visual_aid_app/MainActivity.java
@@ -1,80 +1,169 @@
 package com.example.visual_aid_app;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    SurfaceView mCameraView ;
-    CameraSource mCameraSource;
-    TextView mTextView;
+    private SurfaceView mCameraView;
+    private TextView mTextView;
+    private CameraSource mCameraSource;
+    private TextToSpeech textToSpeech;
+    private TextRecognizer textRecognizer;
+    private final int cameraPermissionID = 101;
+    FirebaseFunctions mFunctions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setViews();
-        startCameraSource();
-    }
 
-    private void setViews() {
         mCameraView = findViewById(R.id.surfaceView);
         mTextView = findViewById(R.id.text_view);
+
+        // Init TextToSpeech and set language
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.US);
+                }
+            }
+        });
+
+
+        mFunctions = FirebaseFunctions.getInstance();
+        startCamera();
     }
 
 
-    private void startCameraSource() {
+    /**
+     * Starts camera source after camera permission is granted
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == getCameraPermissionID()) {
 
-        //Create the TextRecognizer
-         TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+            if (checkHasCameraPermission()) {
 
-        if (!textRecognizer.isOperational()) {
-            Log.w(TAG, "Detector dependencies not loaded yet");
-        } else {
+                Log.i("onRequestResult", "Permission has been granted");
+                try {
+                    mCameraSource.start(mCameraView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-            //Initialize camerasource to use high resolution and set Autofocus on.
-            mCameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+    }
+
+
+    /**
+     * Gets TextBlock from TextRecognizer, set Text to TextView
+     * and Speaks it if listen button is clicked
+     */
+    private void detectText(){
+
+        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+            @Override
+            public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                final SparseArray<TextBlock> items = detections.getDetectedItems();
+                if (items.size() != 0 ){
+                    mTextView.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            //Gets strings from TextBlock and adds to StringBuilder
+                            final StringBuilder stringBuilder = new StringBuilder();
+                            for(int i=0; i<items.size(); i++)
+                                stringBuilder.append(items.valueAt(i).getValue());
+
+                            //Set Text to screen and speaks it if button clicked
+                            mTextView.setText(stringBuilder.toString());
+                            textToSpeech.speak(stringBuilder.toString(), TextToSpeech.QUEUE_FLUSH, null);
+                         /*   findViewById(R.id.voice).setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Log.i("OnClickListener","Text is reading");
+                                    textToSpeech.speak(stringBuilder.toString(), TextToSpeech.QUEUE_FLUSH, null);
+                                }
+                            });*/
+                        }
+                    });
+                }
+            }
+            @Override
+            public void release() {
+            }
+        });
+    }
+
+    /**
+     * Init camera source with needed properties,
+     * then set camera view to surface view.
+     */
+    private void startCamera() {
+
+        textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        if (textRecognizer.isOperational()) {
+
+            mCameraSource = new CameraSource.Builder(getApplicationContext(),textRecognizer)
                     .setFacing(CameraSource.CAMERA_FACING_BACK)
                     .setRequestedPreviewSize(1280, 1024)
                     .setAutoFocusEnabled(true)
                     .setRequestedFps(2.0f)
                     .build();
 
-            /**
-             * Add call back to SurfaceView and check if camera permission is granted.
-             * If permission is granted we can start our cameraSource and pass it to surfaceView
-             */
+            //If permission is granted cameraSource started and passed it to surfaceView
             mCameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
                 @Override
                 public void surfaceCreated(SurfaceHolder holder) {
-                    try {
+                    if(checkHasCameraPermission()){
 
-                        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
-                                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-
-                            requestPermission();
-                            return;
+                        try {
+                            mCameraSource.start(mCameraView.getHolder());
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-                        mCameraSource.start(mCameraView.getHolder());
-                    } catch (IOException e) {
-                        e.printStackTrace();
+
+                    }
+                    else {
+
+                        Log.i("surfaceCreated","Permission request sent");
+                       requestCameraPermission();
                     }
                 }
 
@@ -82,53 +171,97 @@ public class MainActivity extends AppCompatActivity {
                 public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
                 }
 
-                /**
-                 * Release resources for cameraSource
-                 */
                 @Override
                 public void surfaceDestroyed(SurfaceHolder holder) {
                     mCameraSource.stop();
                 }
             });
+            final Handler handler = new Handler();
+            final int delay = 1000;
 
-            //Set the TextRecognizer's Processor.
-            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
-                @Override
-                public void release() {
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    detectText();
+                    handler.postDelayed(this, delay);
+
                 }
+            }, delay);
 
-                /**
-                 * Detect all the text from camera using TextBlock and the values into a stringBuilder
-                 * which will then be set to the textView.
-                 * */
-                @Override
-                public void receiveDetections(Detector.Detections<TextBlock> detections) {
-                    final SparseArray<TextBlock> items = detections.getDetectedItems();
-                    if (items.size() != 0 ){
 
-                        mTextView.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                StringBuilder stringBuilder = new StringBuilder();
-                                for(int i=0;i<items.size();i++){
-                                    TextBlock item = items.valueAt(i);
-                                    stringBuilder.append(item.getValue());
-                                    stringBuilder.append("\n");
-                                }
-                                mTextView.setText(stringBuilder.toString());
-                            }
-                        });
-                    }
-                }
-            });
         }
     }
 
+    private Task<JsonElement> annotateImage(String requestJson) {
+        return mFunctions
+                .getHttpsCallable("annotateImage")
+                .call(requestJson)
+                .continueWith(new Continuation<HttpsCallableResult, JsonElement>() {
+                    @Override
+                    public JsonElement then(@NonNull Task<HttpsCallableResult> task) {
+                        // This continuation runs on either success or failure, but if the task
+                        // has failed then getResult() will throw an Exception which will be
+                        // propagated down.
+                        return JsonParser.parseString(new Gson().toJson(task.getResult().getData()));
+                    }
+                });
+    }
 
-    private void requestPermission()
+    public void createJSONRequest (String base64encoded)
     {
-        int PERMISSION_CODE= 200;
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},PERMISSION_CODE);
+        // Create json request to cloud vision
+        JsonObject request = new JsonObject();
+        // Add image to request
+        JsonObject image = new JsonObject();
+        image.add("content", new JsonPrimitive(base64encoded));
+        request.add("image", image);
+        //Add features to the request
+        JsonObject feature = new JsonObject();
+        feature.add("type", new JsonPrimitive("TEXT_DETECTION"));
+        // Alternatively, for DOCUMENT_TEXT_DETECTION:
+        //feature.add("type", new JsonPrimitive("DOCUMENT_TEXT_DETECTION"));
+        JsonArray features = new JsonArray();
+        features.add(feature);
+        request.add("features", features);
+        //Optionally, provide language hints to assist with language detection
+        JsonObject imageContext = new JsonObject();
+        JsonArray languageHints = new JsonArray();
+        languageHints.add("en");
+        imageContext.add("languageHints", languageHints);
+        request.add("imageContext", imageContext);
 
+    }
+
+    @Override
+    public void onPause(){
+        if(textToSpeech !=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
+    }
+
+    /**
+     * Check whether camera permission is granted
+     * @return true if camera permission is granted, false otherwise
+     */
+    boolean checkHasCameraPermission() {
+
+        return ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /**
+     * Creates a camera permission request
+     */
+    void requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.CAMERA},
+                cameraPermissionID
+        );
+    }
+
+    int getCameraPermissionID() {
+        return cameraPermissionID;
     }
 }
---
+++ app/src/main/java/com/example/visual_aid_app/Util.java
@@ -0,0 +1,36 @@
+package com.example.visual_aid_app;
+
+import android.graphics.Bitmap;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class Util {
+
+    public Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+
+    public String bitmapToBASE64(Bitmap bitmap)
+    {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imageBytes, Base64.NO_WRAP);
+    }
+}
